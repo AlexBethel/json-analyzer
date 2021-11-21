9b4b55097bda77c674de80f168798b200133399e
Alex Bethel <abethel@cs.nmt.edu>
2021-11-21 12:23:16 -0600

    Data type declarations

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::path::Path;
+//! A simple program for generating data structure declarations from a
+//! JSON file.
+
+use std::{collections::HashMap, path::Path};
 
 use clap::Arg;
+use json::JsonValue;
 
 fn main() {
     let app = clap::App::new("json-analyzer")
@@ -29,5 +33,67 @@ fn main() {
         }
     };
 
-    println!("{}", data);
+    let typ = DataType::from_json_value(&data);
+    println!("{:?}", typ);
+}
+
+/// Types of data in a JSON structure.
+#[derive(Debug, PartialEq, Eq)]
+enum DataType {
+    /// Data that is always Null. In practice, this is usually
+    /// combined with `Variant` to create an optional value.
+    Null,
+
+    /// A string of characters.
+    String,
+
+    /// A number that must always be an integer.
+    Int,
+
+    /// A number that can be either a float or an integer.
+    Float,
+
+    /// A boolean.
+    Bool,
+
+    /// A heterogeneous data structure with named elements, like a
+    /// struct.
+    Object(HashMap<String, DataType>),
+
+    /// An array of elements with the same type.
+    Array(Box<DataType>),
+
+    /// One of several possible types.
+    Variant(Vec<DataType>),
+}
+
+impl DataType {
+    /// Generate a data type that could represent something of this
+    /// type, or of the `other` type.
+    pub fn unify(self, _other: DataType) -> Self {
+        todo!()
+    }
+
+    /// Create a data type that can reprent the given value.
+    pub fn from_json_value(v: &JsonValue) -> Self {
+        match v {
+            JsonValue::Null => Self::Null,
+            JsonValue::Short(_) => Self::String,
+            JsonValue::String(_) => Self::Int,
+            JsonValue::Number(_) => todo!(),
+            JsonValue::Boolean(_) => Self::Bool,
+            JsonValue::Object(obj) => Self::Object(
+                obj.iter()
+                    .map(|(key, value)| (key.to_string(), Self::from_json_value(value)))
+                    .collect(),
+            ),
+            JsonValue::Array(elems) => Self::Array(Box::new(
+                elems
+                    .iter()
+                    .map(Self::from_json_value)
+                    .reduce(Self::unify)
+                    .unwrap_or(Self::Variant(Vec::new())),
+            )),
+        }
+    }
 }
